b11bee7bf1fbcf8640279601d44ec803406b4634
Leah <amelia_rawls@ccpsnet.net>
2022-03-18 13:45:00 -0400

    window prompts for input

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,24 @@
 
 
 // prompt for length of password between 8 and 128 characters
+
+window.prompt("Your password needs to be 8 - 128 characters long. How long do you want your password? ");
 // validate prompt
 
 // do you want lowercase letters
+window.prompt("Do you want to include lowercase letters? Type YES or NO.");
 // validate prompt
 
 // do you want uppercase letters
+window.prompt("Do you want to include uppercase letters? Type YES or NO.");
 // validate prompt
 
 // do you want numbers
+window.prompt("Do you want to include numbers? Type YES or NO.");
 // validate prompt
 
 // do you want special characters
+window.prompt("Do you want to include special characters? Type YES or NO.");
 // validate prompt
 
 // generate password
@@ -22,20 +28,21 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //passwordText.value = password;
 
-}
+//}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
 
 
 /* RUN GAME */
-generatePassword();
+//generatePassword();
+
