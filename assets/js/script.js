62799b0e59f0eb8b02e76b7f912e3ae865b21b9d
Leah <amelia_rawls@ccpsnet.net>
2022-03-18 22:17:30 -0400

    added conditionals for window prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,37 +18,107 @@ var generatePassword = function() {
   var lowercaseLetters = function() {
     var lowercase = window.prompt('Do you want to include lowercase letters? Type "YES" or "NO".');
     console.log(lowercase);
-    //convert lowercase to all lowercase letters
     
       // validate prompt
       if (lowercase === "" || lowercase === null) {
         window.alert("You need to provide a valid answer.");
         return lowercaseLetters();
       } 
-      
+      //convert lowercase to all lowercase letters
       lowercase = lowercase.toLowerCase();
 
       if (lowercase === "yes") {
         window.alert("You DO want to include lowercase letters.");
 
       } else if (lowercase === "no") {
-        window.alert("You DO NOT want to include lowercase letters");
+        window.alert("You DO NOT want to include lowercase letters.");
 
       } else {
+        window.alert("You need to provide a valid answer.");
         return lowercaseLetters();
       }
+      uppercaseLetters();
     }
 
   // do you want uppercase letters
-  window.prompt("Do you want to include uppercase letters? Type YES or NO.");
-  // validate prompt
+  var uppercaseLetters = function() {
+    var uppercase = window.prompt('Do you want to include uppercase letters? Type "YES" or "NO".');
+    console.log(uppercase);
+    
+      // validate prompt
+      if (uppercase === "" || uppercase === null) {
+        window.alert("You need to provide a valid answer.");
+        return uppercaseLetters();
+      } 
+      //convert uppercase to all lowercase letters
+      uppercase = uppercase.toLowerCase();
+
+      if (uppercase === "yes") {
+        window.alert("You DO want to include uppercase letters.");
+
+      } else if (uppercase === "no") {
+        window.alert("You DO NOT want to include uppercase letters.");
+
+      } else {
+        window.alert("You need to provide a valid answer.");
+        return uppercaseLetters();
+      }
+      number();
+    }
 
   // do you want numbers
-  window.prompt("Do you want to include numbers? Type YES or NO.");
+  var number = function() {
+    var num1 = window.prompt("Do you want to include numbers? Type YES or NO.");
+    console.log(num1);
+    
+      // validate prompt
+      if (num1 === "" || num1 === null) {
+        window.alert("You need to provide a valid answer.");
+        return number();
+      } 
+      //convert uppercase to all lowercase letters
+      num1 = num1.toLowerCase();
+
+      if (num1 === "yes") {
+        window.alert("You DO want to include numbers.");
+
+      } else if (num1 === "no") {
+        window.alert("You DO NOT want to include numbers.");
+
+      } else {
+        window.alert("You need to provide a valid answer.");
+        return number();
+      }
+      SpecialCharacters();
+    }
+  
   // validate prompt
 
   // do you want special characters
-  window.prompt("Do you want to include special characters? Type YES or NO.");
+  var SpecialCharacters = function() {
+    var character = window.prompt("Do you want to include special characters? Type YES or NO.");
+    console.log(character);
+    
+      // validate prompt
+      if (character === "" || character === null) {
+        window.alert("You need to provide a valid answer.");
+        return SpecialCharacters();
+      } 
+      //convert uppercase to all lowercase letters
+      character = character.toLowerCase();
+
+      if (character === "yes") {
+        window.alert("You DO want to include numbers.");
+
+      } else if (character === "no") {
+        window.alert("You DO NOT want to include numbers.");
+
+      } else {
+        window.alert("You need to provide a valid answer.");
+        return SpecialCharacters();
+      }
+    }
+  
   // validate prompt
 
 // generate password
